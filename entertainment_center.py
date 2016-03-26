6d572491618e93fedebabcaa655837aeaeef4be6
unknown <dont@emailme.ever>
2016-03-26 14:47:18 -0400

    Renamed the module that contains the movie class and updated the object creation statements in entertainment_center.py accordingly. Removed the sample movie objects and created my own list of movie objects.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,9 +1,12 @@
-import movieClass, fresh_tomatoes
+import media, fresh_tomatoes
 
-toy_story = movieClass.Movie("Toy Story", "A story about toys coming to life", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-avatar = movieClass.Movie("Avatar", "A twin goes to another planet and helps the people there fight against the bad guys", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-edward_scissorhands = movieClass.Movie("Edward Scissorhands", "A boy with scissors for fingers goes to live in a town", "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Edwardscissorhandsposter.JPG/220px-Edwardscissorhandsposter.JPG", "https://www.youtube.com/watch?v=eq2PPFUhfpo")
+edward_scissorhands = media.Movie("Edward Scissorhands", "A gentle man, with scissors for hands, is brought into a new community after living in isolation.", "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Edwardscissorhandsposter.JPG/220px-Edwardscissorhandsposter.JPG", "https://www.youtube.com/watch?v=eq2PPFUhfpo")
+moulin_rouge = media.Movie("Moulin Rouge", "A poet falls for a beautiful courtesan whom a jealous duke covets in this stylish musical, with music drawn from familiar 20th century sources.", "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Moulin_rouge_poster.jpg/220px-Moulin_rouge_poster.jpg", "https://www.youtube.com/watch?v=dtEgAx80NC4")
+apollo_13 = media.Movie("Apollo 13","NASA must devise a strategy to return Apollo 13 to Earth safely after the spacecraft undergoes massive internal damage putting the lives of the three astronauts on board in jeopardy.","https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Apollo_thirteen_movie.jpg/220px-Apollo_thirteen_movie.jpg","https://www.youtube.com/watch?v=nEl0NsYn1fU")
+ace_ventura_2 = media.Movie("Ace Ventura: When Nature Calls","Ace Ventura, Pet Detective, returns from a spiritual quest to investigate the disappearance of a rare white bat, the sacred animal of a tribe in Africa.","https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/AceVenturaWhenNatureCallsposter.jpg/220px-AceVenturaWhenNatureCallsposter.jpg","https://www.youtube.com/watch?v=A99O84ct-WM")
+pans_labyrinth = media.Movie("Pan's Labyrinth","In the falangist Spain of 1944, the bookish young stepdaughter of a sadistic army officer escapes into an eerie but captivating fantasy world.","https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Pan%27s_Labyrinth.jpg/220px-Pan%27s_Labyrinth.jpg","https://www.youtube.com/watch?v=EqYiSlkvRuw")
 
-movies = [toy_story, avatar, edward_scissorhands]
+
+movies = [edward_scissorhands, moulin_rouge, apollo_13, ace_ventura_2, pans_labyrinth]
 
 fresh_tomatoes.open_movies_page(movies)
