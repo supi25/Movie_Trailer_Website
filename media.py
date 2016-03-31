755123aa2b53e14754e733029345454accdc8ff5
unknown <dont@emailme.ever>
2016-03-30 22:14:07 -0400

    Added a list of starring actors and actresses to the movie class and modified fresh_tomatoes to display this list.

--- media.py
+++ media.py
@@ -1,12 +1,13 @@
 import webbrowser
 
 class Movie():
-    def __init__(self, movie_title, movie_storyline, release_year, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, release_year, poster_image, trailer_youtube, stars=[]):
         self.title = movie_title
         self.storyline = movie_storyline
         self.year = release_year
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        self.stars = stars
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
